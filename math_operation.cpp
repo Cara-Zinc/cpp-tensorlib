d5234fa3c39a6dad2227e29181a02c56d2e61f72
Cara-Zinc <zincala1084@gmail.com>
2024-01-15 01:23:40 +0800

    fix some bug

--- math_operation.cpp
+++ math_operation.cpp
@@ -73,7 +73,7 @@ namespace ts
         Tensor result(result_shape, a.type());
 
         // Iterate over elements in the broadcasted shape
-        for (size_t i = 0; i < result.totalSize(); ++i)
+        for (size_t i = 0; i < result.total_size(); ++i)
         {
             size_t idx_a = calculateBroadcastIndex(i, a.shape, a.stride);
             size_t idx_b = calculateBroadcastIndex(i, b.shape, b.stride);
